a2e66e54fcfa1719b4e13f14562766c2e7d0553b
John Kutch <jkutch2@yahoo.com>
2023-01-16 18:24:43 -0500

    added required questions

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please give a description of your project?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What does the user need to install to run the project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What should a user know about using the project?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How does the user contribute to the project repo?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What should the user know about tests for the project?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['APACHE 2.0', 'BSD 3', 'GPL 3.0', 'MIT', 'None']
+    },
     {
         type: 'input',
         name: 'github',
@@ -13,24 +50,12 @@ const questions = [
         name: 'email',
         message: 'What is your email address?'
     },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is your project\'s name?'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'What kind of license should your project have?',
-        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
-    },
-
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) =>
-        err ? console.error(err) : console.log(`Success Your README.md file has been created.`)
+        err ? console.error(err) : console.log(`Success! Your README.md file has been created.`)
     );
 }
 
