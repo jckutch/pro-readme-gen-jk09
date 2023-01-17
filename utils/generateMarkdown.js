ca3c9c391674afee415ca8693b9cbd1395eb2773
John Kutch <116672999+jckutch@users.noreply.github.com>
2023-01-17 11:29:36 -0500

    add license links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,19 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  console.log(license);
+  const licenseLinks = {
+    Apache: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)', 
+    GPL: '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)',
+    MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)', 
+    Mozilla:'[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)', 
+    None: ''
+
+  }
+  console.log(licenseLinks[license]);
+  return licenseLinks[license];
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
